3253f8bde8ecb344c9fd20f6b7c537b53568e576
Amgad Abdullah <amga200d@gmail.com>
2025-05-20 10:40:04 -0600

    Added a Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Suspense, lazy } from "react";
 
 // Lazy-loaded page components
-// const HomePage = lazy(() => import("./pages/HomePage"));
+const HomePage = lazy(() => import("./pages/HomePage"));
 // const RegisterPage = lazy(() => import("./pages/RegisterPage"));
 const LoginPage = lazy(() => import("./pages/LoginPage"));
 // const DashboardPage = lazy(() => import("./pages/DashboardPage"));
@@ -26,8 +26,8 @@ function App() {
     <BrowserRouter>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          {/* <Route path="/" element={<HomePage />} />
-          <Route path="/register" element={<RegisterPage />} /> */}
+          <Route path="/" element={<HomePage />} />
+          {/* <Route path="/register" element={<RegisterPage />} /> */}
           <Route path="/login" element={<LoginPage />} />
           {/* <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/profile" element={<ProfilePage />} />
