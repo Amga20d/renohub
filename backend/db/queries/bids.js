5e7e94823b0b4f41778344fd2e2f5650ea5bc36d
Amgad Abdullah <amga200d@gmail.com>
2025-05-26 12:28:42 -0600

    modified db schema and made relevent changes to account for that

--- backend/db/queries/bids.js
+++ backend/db/queries/bids.js
@@ -10,46 +10,57 @@ const createBid = (user_id, newBid) => {
     status,
     notes,
     created_at
-  } = newBid
+  } = newBid;
 
   return db
-  .query('INSERT INTO Bids (project_id, user_id, amount, status, notes, created_at) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;',
-    [project_id, user_id, amount, status, notes , created_at]
-  ).then((data => data.rows[0]));
+    .query(
+      'INSERT INTO Bids (project_id, user_id, amount, status, notes, created_at) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;',
+      [project_id, user_id, amount, status, notes, created_at]
+    )
+    .then((data) => data.rows[0]);
 };
 
 const getAllBids = () => {
   return db.query('SELECT * FROM Bids;').then((data) => data.rows);
-}
+};
 
 const getBidById = (id) => {
   return db
-  .query('SELECT * FROM Bids WHERE id = $1;', [id])
-  .then((data) => data.rows[0]);
+    .query('SELECT * FROM Bids WHERE id = $1;', [id])
+    .then((data) => data.rows[0]);
 };
 
 const getBidsByUserId = (user_id) => {
   return db
-  .query('SELECT * FROM Bids WHERE user_id = $1;', [user_id])
-  .then((data) => data.rows);
+    .query('SELECT * FROM Bids WHERE user_id = $1;', [user_id])
+    .then((data) => data.rows);
 };
 
 const updateBid = (id, updatedBid) => {
   const {
     amount,
-    notes
+    notes,
+    status
   } = updatedBid;
 
   return db
-  .query('UPDATE Bids SET amount = $2, notes = $3 WHERE id = $1 RETURNING *;',
-    [id, amount, notes]
-  ).then((data) => data.rows[0]);
-}
+    .query('UPDATE Bids SET amount = $2, notes = $3, status = $4 WHERE id = $1 RETURNING *;',
+      [id, amount, notes, status]
+    )
+    .then((data) => data.rows[0]);
+};
 
 const removeBid = (id) => {
   return db
-  .query('DELETE FROM Bids WHERE id = $1;', [id])
-  .then((data) => data.rows);
+    .query('DELETE FROM Bids WHERE id = $1;', [id])
+    .then((data) => data.rows);
 };
 
-module.exports = { createBid, getAllBids, getBidById, getBidsByUserId, updateBid, removeBid};
+module.exports = {
+  createBid,
+  getAllBids,
+  getBidById,
+  getBidsByUserId,
+  updateBid,
+  removeBid
+};
